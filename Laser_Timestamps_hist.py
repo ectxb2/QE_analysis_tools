8995f1eceb546efba4da12d4d00933abcbcc1dc2
ectxb2 <trillere@msu.edu>
2023-07-11 11:45:33 -0400

    change time axis

--- Laser_Timestamps_hist.py
+++ Laser_Timestamps_hist.py
@@ -16,6 +16,7 @@ mask7 = pkts['packet_type'] == 7 # this is ext. trig.
 mask0 = pkts['packet_type'] == 0 # this is data packet?
 pkts0 = pkts[mask0]
 pkts7 = pkts[mask7]
+print(len(pkts7))
 
 timestamps0 = pkts0['timestamp']
 timestamps7 = pkts7['timestamp']
@@ -34,24 +35,36 @@ for t in timestamps7:
 
 #shift times to line up and remove 40000 clock cycles after pulse (laser prpbably not there)
 shifted_ts = []
-#print(len(timestamps7))
+shifted_ts7 = []
+print(len(timestamps7))
 for i in range(0,(len(timestamps7)-1)):
     mask1 = timestamps0 > (timestamps7[i]+300000) #500,000 CS between pulses
     timestampsnew = timestamps0[mask1]
     mask2 = timestampsnew < timestamps7[i+1]
-    timestampsnew1 = timestampsnew[mask2]-delta_t7[i]
+    timestampsnew1 = timestampsnew[mask2]-(delta_t7[i]+timestamps7[0]+500000)
     #print(timestamps7[i])
     #print( 'delta_t7 = ' + str(delta_t7[i]))
     #print(len(timestampsnew1))
     shifted_ts = np.append(shifted_ts, timestampsnew1)
+    newts7 = timestamps7[i+1]-delta_t7[i]-timestamps7[0]-500000
+    shifted_ts7 = np.append(shifted_ts7,newts7)
     
+
+print (len(shifted_ts7))
 n, bins = np.histogram(shifted_ts, bins = 1000)
 max_bin_time = bins[np.where(n == (max(n)))[0][0]]
 plt.axvline(x = max_bin_time, color = 'r', label = 'Max Counts at '+str(max_bin_time))
-plt.stairs(n, bins)
-plt.xlabel('Clock Cycles 100ns (10MHx)')
-plt.ylabel('Counts')
+plt.stairs(n, bins, label = 'Data')
+
+#n7, bins = np.histogram(shifted_ts7, bins = bins)
+n7, bins = np.histogram(shifted_ts7, bins = 10)
+plt.stairs(n7, bins, label = 'Triggers')
+#print(n7[-1])
+
+plt.xlabel('Clock Cycles till next trigger, 100ns (10MHx)')
+plt.ylabel('Summed Counts')
 plt.legend()
+plt.title('Time delay from Calibration Data to Trigger Signal Summed Data Counts')
 current_values = plt.gca().get_xticks()
 #plt.gca().set_xticklabels(['{:.0f}'.format(y) for y in current_values])
 plt.show()
